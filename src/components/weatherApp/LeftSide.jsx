9ac688ff98722c3c879fd82440f71a6b5068220c
elham saremi <elhamsaremi88@gmail.com>
2023-11-15 23:27:20 +0330

    show response of request in ui

--- src/components/weatherApp/LeftSide.jsx
+++ src/components/weatherApp/LeftSide.jsx
@@ -1,32 +1,53 @@
 import bgIage from "../../images/e06426df1254df03a60b913eaf1f97dd.png";
 import location from "../../images/Location.svg";
-import sunny from "../../images/Sun2.svg";
+import moment from "moment";
 
-export const LeftSide = () => {
+export const LeftSide = ({ weatherData, error, loading }) => {
   return (
     <div className="gradient rounded-2xl flex-1 relative text-white">
       <img
         src={bgIage}
-        alt="bg-image"
+        alt="background"
         className=" mix-blend-soft-light rounded-2xl w-full "
       />
-      <div className="absolute top-8 left-8 flex flex-col gap-4 items-start justify-between h-[80%]">
-        <div className="flex flex-col items-center justify-start gap-2">
-          <div className="text-2xl font-bold">Tuesday</div>
-          <div className="text-sm font-light flex items-center">
-            20 Jun 2022
+      {loading && <div className="absolute top-8 left-8">Loading...</div>}
+      {error && (
+        <div className="absolute top-8 left-8 text-red-500">{error}</div>
+      )}
+      {weatherData && (
+        <div className="absolute top-8 left-8 flex flex-col gap-4 items-start justify-between h-[80%]">
+          <div className="flex flex-col items-start justify-start gap-2">
+            <div className="text-2xl font-bold">
+              {/* {new Date().toLocaleDateString("en-US", { weekday: "long" })} */}
+              {moment.unix(weatherData.dt).format("dddd")}
+            </div>
+            <div className="text-sm font-light flex items-center">
+              {/* {new Date().toLocaleDateString("en-US", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+              })} */}
+              {moment.unix(weatherData.dt).format("MMM DD, YYYY")}
+            </div>
+            <div className="text-sm font-light flex items-center justify-between">
+              <img src={location} alt="icon" width={20} />
+              {`${weatherData.name}, ${weatherData.sys.country}`}
+            </div>
           </div>
-          <div className="text-sm font-light flex items-center justify-between">
-            <img src={location} alt="icon" width={20} />
-            Biarritz, FR
+          <div className="flex flex-col items-start gap-3">
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+              alt="icon"
+              width={70}
+              className="flex"
+            />
+            <div className="text-3xl font-bold">
+              {(weatherData.main.temp - 273.15).toFixed(2)} °C
+            </div>
+            <div className="text-xl">{weatherData.weather[0].main}</div>
           </div>
         </div>
-        <div className="flex flex-col items-center gap-3">
-          <img src={sunny} alt="icon" width={70} className="flex" />
-          <div className="text-3xl font-bold">29 °C</div>
-          <div className="text-xl font-bold">Sunny</div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/weatherApp/RightSide.jsx
+++ src/components/weatherApp/RightSide.jsx
@@ -1,44 +1,62 @@
+import moment from "moment";
 import sunny from "../../images/Sun.svg";
-export const RightSide = () => {
+import bi_cloud_rain from "../../images/bi_cloud-rain.svg";
+import bi_cloud_sun from "../../images/bi_cloud-sun.svg";
+
+export const RightSide = ({ weatherData }) => {
   return (
     <div className="bg-[#222831] flex flex-1 flex-col rounded-tr-2xl rounded-br-2xl">
-      <div className="flex items-center justify-between text-white px-8 pt-8">
-        <div>PRECIPITATION</div>
-        <div>0%</div>
-      </div>
-      <div className="flex items-center justify-between text-white px-8">
-        <div>HUMIDITY</div>
-        <div>42%</div>
-      </div>
-      <div className="flex items-center justify-between text-white px-8">
-        <div>WIND</div>
-        <div>3 km/h</div>
-      </div>
-      <div className="flex items-center justify-center gap-[1px] mt-16 mb-44">
-        <div className="flex flex-col items-center justify-center gap-4 bg-white rounded-lg p-4">
-          <img width={30} src={sunny} alt="icon" />
-          <div className="text-xs">Tue</div>
-          <div className="text-sm font-bold">30 °C</div>
-        </div>
+      {weatherData && (
+        <>
+          <div className="flex items-center justify-between text-white px-8 pt-8">
+            {/* <div>PRECIPITATION</div> */}
+            <div>PRESSURE</div>
+            <div>{weatherData.main.pressure}</div>
+          </div>
+          <div className="flex items-center justify-between text-white px-8">
+            <div>HUMIDITY</div>
+            <div>{weatherData.main.humidity} %</div>
+          </div>
+          <div className="flex items-center justify-between text-white px-8">
+            <div>WIND</div>
+            <div>{weatherData.wind.speed} km/h</div>
+          </div>
 
-        <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
-          <img width={30} src={sunny} alt="icon" />
-          <div className="text-xs">Tue</div>
-          <div className="text-sm font-bold">30 °C</div>
-        </div>
+          <div className="flex items-center justify-center gap-[1px] mt-16 mb-44">
+            <div className="flex flex-col items-center justify-center gap-4 bg-white rounded-lg px-1 py-4">
+              <img
+                width={30}
+                src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                alt="icon"
+              />
+              <div className="text-xs">
+                {moment.unix(weatherData.dt).format("dddd")}
+              </div>
+              <div className="text-sm font-bold">
+                {(weatherData.main.temp - 273.15).toFixed(2)} °C
+              </div>
+            </div>
 
-        <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
-          <img width={30} src={sunny} alt="icon" />
-          <div className="text-xs">Tue</div>
-          <div className="text-sm font-bold">30 °C</div>
-        </div>
+            <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
+              <img width={30} src={bi_cloud_rain} alt="icon" />
+              <div className="text-xs">Thu</div>
+              <div className="text-sm font-bold">30 °C</div>
+            </div>
 
-        <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
-          <img width={30} src={sunny} alt="icon" />
-          <div className="text-xs">Tue</div>
-          <div className="text-sm font-bold">30 °C</div>
-        </div>
-      </div>
+            <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
+              <img width={30} src={bi_cloud_sun} alt="icon" />
+              <div className="text-xs">Fry</div>
+              <div className="text-sm font-bold">30 °C</div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-4 bg-[#272E37] rounded-lg p-4">
+              <img width={30} src={sunny} alt="icon" />
+              <div className="text-xs">Sat</div>
+              <div className="text-sm font-bold">30 °C</div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
